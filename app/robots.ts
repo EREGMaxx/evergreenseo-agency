0ba4fd8094d62bb12d46b48792ef996d9ba003b4
EREGMaxx <maxx@evergreenseo.agency>
2026-05-10 12:27:48 -0700

    fix: add Disallow /proposals/ to robots.ts (programmatic robots overrides static file)

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/proposals/",
     },
     sitemap: "https://evergreenseo.agency/sitemap.xml",
   };
